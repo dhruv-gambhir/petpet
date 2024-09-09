5cd5c7469071b8668c06182d4694d127510766f8
jingjietan <jingjienat@gmail.com>
2024-09-09 23:45:58 +0800

    main event page

---
+++ client/app/events/EventCard.js
@@ -0,0 +1,29 @@
+
+
+export default function EventCard({ event }) {
+  return (
+    <div className="relative bg-white rounded w-4/5 h-80 ml-8 flex flex-row p-3 gap-8 shadow-sm shadow-black">
+      <div className="flex-none relative">
+        <img className="object-scale-down max-h-full aspect-auto" src="https://images.dog.ceo/breeds/pyrenees/n02111500_5225.jpg" alt="A dog photo." />
+        <div className="absolute w-4/5 text-center bg-gray-300 px-2 py-1 rounded z-50 text-nowrap shadow-sm shadow-black bottom-[-1rem] left-[50%] translate-x-[-50%]">
+          <p>{event.coordinator}</p>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col justify-between">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-lg">{event.name}</h2>
+          <p className="line-clamp-[8]">{event.description}</p>
+        </div>
+        <div className="flex flex-row gap-4">
+          <p className="bg-gray-300 px-2 py-1 rounded text-nowrap">{event.date}</p>
+          <p className="bg-gray-300 px-2 py-1 rounded text-nowrap">{event.time}</p>
+          <p className="bg-gray-300 px-2 py-1 rounded text-nowrap">{event.location}</p>
+        </div>
+      </div>
+      <div className="flex-initial flex flex-row items-start flex-nowrap">
+        <button className="p-2 font-bold hover:underline">Interested</button>
+        <button className="p-2 hover:drop-shadow hover:shadow-gray-300">❤️</button>
+      </div>
+    </div>
+  )
+}
