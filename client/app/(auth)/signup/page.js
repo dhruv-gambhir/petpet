56a1c13363df48f65db54655013b715f0fbaaa47
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2024-09-24 23:16:26 +0800

    register as page done

--- client/app/(auth)/signup/page.js
+++ client/app/(auth)/signup/page.js
@@ -1,16 +1,21 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SignUp() {
     const router = useRouter();
 
     return(
-        <main className="flex flex-row w-full h-full">
-            <button onClick={() => router.push("/signup/user")} className="w-2/6 h-2/4">
+        <main className="flex flex-col w-full h-full items-center justify-center">
+            <p className="text-mypurple text-3xl mt-8">Register As</p>
+            <div className="flex flex-row w-full h-full items-center justify-center">
+            <button onClick={() => router.push("/signup/user")} className="w-2/6 h-2/4 bg-mypurple text-white text-3xl rounded m-8">
                 User
             </button>
+            <button onClick={() => router.push("/signup/agency")} className="w-2/6 h-2/4 bg-mypurple text-white text-3xl rounded m-8">
+                Agency
+            </button>
+            </div>
         </main>
     );
 };
