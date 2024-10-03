969f56c5d25a7ed08ab3443df4529f6379a7a46b
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2024-10-03 14:50:47 +0800

    updated store.js

--- client/app/store.js
+++ client/app/store.js
@@ -1,23 +1,22 @@
-import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 const useStore = create(
   persist(
     (set) => ({
       zIsLoggedIn: false,
-      zEmail: '',
+      zEmail: "",
       zLogin: (email) => set({ zIsLoggedIn: true, zEmail: email }),
-      zLogout: () => set({ zIsLoggedIn: false, zEmail: '' }),
+      zLogout: () => set({ zIsLoggedIn: false, zEmail: "" }),
     }),
     {
-      name: 'petpal-auth-storage',
+      name: "petpal-auth-storage",
       storage:
-        typeof window !== 'undefined'
+        typeof window !== "undefined"
           ? createJSONStorage(() => localStorage)
           : undefined,
-    }
-  )
+    },
+  ),
 );
 
 export default useStore;
-
