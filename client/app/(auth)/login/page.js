740372a33f226b849503c0c4aa3384945a850dad
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2024-10-04 13:03:26 +0800

    Redirect from "/" to "/login"

--- client/app/(auth)/login/page.js
+++ client/app/(auth)/login/page.js
@@ -6,61 +6,63 @@ import { useRouter } from "next/navigation";
 import useStore from "../../store";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const router = useRouter();
-    const { zLogin } = useStore();
-    const handleLogIn = async (event) => {
-        event.preventDefault();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const { zLogin } = useStore();
 
-        try {
-            const user = await login(email, password);
-            zLogin(email);
-            router.push("/profile");
-        } catch (error) {
-            alert("Login error:", error.message);
-        }
-    };
+  const handleLogIn = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div className="flex flex-row m-8 justify-center items-center bg-white w-1/2 h-4/6">
-            <form className="m-8 flex flex-col justify-center items-center w-2/6 h-4/6 ">
-                <h1 className="color-mypurple text-2xl font-bold">Login</h1>
-                <div className="flex flex-col relative justify-center w-5/6">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="border-2 border-mypurple rounded bg-mybg h-8 m-4"
-                    />
-                </div>{" "}
-                <div className="flex flex-col relative justify-center w-5/6 ">
-                    <input
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="border-2 border-mypurple rounded bg-mybg h-8 m-4"
-                    />
-                </div>{" "}
-                <div className="flex flex-col justify-center items-center">
-                    <button
-                        type="submit"
-                        className="bg-mypurple w-16 m-4 rounded p-2 text-white"
-                        onClick={handleLogIn}
-                    >
-                        Login
-                    </button>
+    try {
+      const user = await login(email, password);
+      zLogin(email);
+      router.push("/profile");
+    } catch (error) {
+      alert("Login error:", error.message);
+    }
+  };
 
-                    <a href="/signup" className="text-lg underline">
-                        New User? Sign Up
-                    </a>
+  return (
+    <div className="flex flex-row m-8 justify-center items-center bg-white w-1/2 h-4/6">
+      <form className="m-8 flex flex-col justify-center items-center w-2/6 h-4/6 ">
+        <h1 className="color-mypurple text-2xl font-bold">Login</h1>
+        <div className="flex flex-col relative justify-center w-5/6">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-2 border-mypurple rounded bg-mybg h-8 m-4"
+          />
+        </div>{" "}
+        <div className="flex flex-col relative justify-center w-5/6 ">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-2 border-mypurple rounded bg-mybg h-8 m-4"
+          />
+        </div>{" "}
+        <div className="flex flex-col justify-center items-center">
+          <button
+            type="submit"
+            className="bg-mypurple w-16 m-4 rounded p-2 text-white"
+            onClick={handleLogIn}
+          >
+            Login
+          </button>
 
-                    <a href="/" className="text-lg underline">
-                        Forgot Password?
-                    </a>
-                </div>
-            </form>
+          <a href="/signup" className="text-lg underline">
+            New User? Sign Up
+          </a>
+
+          <a href="/" className="text-lg underline">
+            Forgot Password?
+          </a>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
