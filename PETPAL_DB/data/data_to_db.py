c145b4183af1f5b6e6fc5b6383122d8db8bf962c
Mishra-Apurva <100474366+Mishra-Apurva@users.noreply.github.com>
2024-10-19 12:37:40 +0800

    fix: postgres table and column names in lowercase

--- PETPAL_DB/data/data_to_db.py
+++ PETPAL_DB/data/data_to_db.py
@@ -12,9 +12,10 @@ def connect_db():
 def load_csv_into_table(engine, table_name, csv_file_path, columns):
     try:
         df = pd.read_csv(csv_file_path)
+        # Convert the DataFrame's column names to lowercase
+        df.columns = df.columns.str.lower()
         if df.shape[1] > len(columns.split(',')):  # Check if there's an extra column
             df.drop(df.columns[0], axis=1, inplace=True)  # Drop the first column (index column)
-
         # Load DataFrame into the database
         df.to_sql(table_name, engine, if_exists='append', index=False)
         print(f"{len(df)} rows of data loaded into {table_name} from {csv_file_path}")
@@ -27,15 +28,15 @@ def main():
 
     # Map each table to its corresponding CSV file and column list
     table_csv_map = {
-        "Users": ("user.csv", "userid, Name, Email, PhoneNumber, CreatedAt, updatedAt, Bio, IsAgency, Address, LicenseNumber, ImageUrl"),
-        "Pets": ("pet.csv", "id, ownerid, name, age, sex, species, breed, createdat, ImageUrl"),
-        "Sitting_Requests": ("sitting.csv", "id, userid, pay, startdate, enddate, status, description, createdat, location, tasktype"),
-        "Sitter_Interests": ("sitting_interest.csv", "id, userid, sittingrequestid, createdat, status"),
-        "Pets_Sitting_Requests": ("pet_sitting_request.csv", "id, sittingrequestid, petid"),
-        "Events": ("event.csv", "id, createdby, event_name, description, location, startdate, createdat, cost, ImageUrl, status"),
-        "Event_Interests": ("event_interest.csv", "id, userid, eventid, createdat, status"),
-        "Adoption_Listings": ("adoption.csv", "id, agentid, petid, description, status, createdat, updatedat"),
-        "Adoption_Interests": ("adoption_interest.csv", "id, userid, adoptionlistingid, createdat, status")
+        "users": ("user.csv", "userid, Name, Email, PhoneNumber, CreatedAt, updatedAt, Bio, IsAgency, Address, LicenseNumber, ImageUrl"),
+        "pets": ("pet.csv", "id, ownerid, name, age, sex, species, breed, createdat, ImageUrl"),
+        "sitting_requests": ("sitting.csv", "id, userid, pay, startdate, enddate, status, description, createdat, location, tasktype"),
+        "sitter_interests": ("sitting_interest.csv", "id, userid, sittingrequestid, createdat, status"),
+        "pets_sitting_requests": ("pet_sitting_request.csv", "id, sittingrequestid, petid"),
+        "events": ("event.csv", "id, createdby, event_name, description, location, startdate, createdat, cost, ImageUrl, status"),
+        "event_interests": ("event_interest.csv", "id, userid, eventid, createdat, status"),
+        "adoption_listings": ("adoption.csv", "id, agentid, petid, description, status, createdat, updatedat"),
+        "adoption_interests": ("adoption_interest.csv", "id, userid, adoptionlistingid, createdat, status")
     }
 
     # Connect to the database
