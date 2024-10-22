66f0d6a5261931b4d38ad54b01b2e220f1dd5648
Samantha Tan <tans4mantha@gmail.com>
2024-10-22 18:13:15 +0800

    Get user's current location.

--- client/app/(webapp)/sitters/sitter/map.js
+++ client/app/(webapp)/sitters/sitter/map.js
@@ -1,50 +1,63 @@
 'use client'
-import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import React, { useEffect, useState, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '500px'
 };
 
-const center = {
-  lat: 1.3876,
-  lng: 103.7454
-};
-
 function GoogleMapView() {
-    const { isLoaded } = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    })
-  
-    const [map, setMap] = React.useState(null)
-  
-    const onLoad = React.useCallback(function callback(map) {
-      const bounds = new window.google.maps.LatLngBounds(center)
-      map.fitBounds(bounds)
-  
-      setMap(map)
-    }, [])
-  
-    const onUnmount = React.useCallback(function callback(map) {
-      setMap(null)
-    }, [])
-  
-    return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
-    ) : (
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, 
+  });
+
+  const [map, setMap] = useState(null);
+  const [center, setCenter] = useState({ lat: 1.3483, lng: 103.6831 }); // Default center at NTU if location services not on
+
+  // Get user's current location and update map center
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          setCenter(userLocation); // Update center with user location
+        },
+        () => {
+          console.log("Unable to retrieve your location");
+        }
+      );
+    }
+  }, []);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, [center]);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
       <></>
-    )
-  }
-  
-  export default React.memo(GoogleMapView)
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(GoogleMapView);
