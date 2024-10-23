dbb6ae5eb5174abc7a9a163e3ea99e54008f250f
jingjietan <jingjienat@gmail.com>
2024-10-23 22:00:14 +0800

    Refactor AdoptionCard component to improve text wrapping and image URL handling

--- client/app/(webapp)/adoption/AdoptionCard.js
+++ client/app/(webapp)/adoption/AdoptionCard.js
@@ -6,7 +6,7 @@ import { registerInterestInAdoption, unregisterInterestInAdoption } from "./adop
 
 const AdoptionTile = ({ label, content, units }) => {
     return (content && (<div className="bg-white border w-20 h-20 flex flex-col justify-center items-center rounded text-nowrap">
-            <p className="text-mypurple">
+            <p className="text-mypurple text-wrap text-center truncate">
                 {content} {units ?? ""}
             </p>
             <span className="text-xs">{label}</span>
@@ -25,7 +25,7 @@ export default function AdoptionCard({ detail, isOrganizer }) {
             <div className="flex-none w-60 relative">
                 <img
                     className="object-scale-down mx-auto h-full aspect-auto"
-                    src={detail.pet?.image_url}
+                    src={detail.pet?.imageurl}
                     alt="A dog photo."
                 />
                 <div className="absolute w-4/5 text-center bg-gray-300 px-2 py-1 rounded z-50 text-nowrap shadow-sm shadow-black bottom-[-1rem] left-[50%] translate-x-[-50%]">
