962bcad2ec24ccfaa6be1db119912e6a43f5b0e1
NithyaH <nithya.edu02@gmail.com>
2024-10-27 21:04:36 +0800

    Add Pet image, Fix minor bugs in profile page

--- client/app/(webapp)/profile/[userId]/addPet.js
+++ client/app/(webapp)/profile/[userId]/addPet.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import useStore from "@/app/store"; 
-
+import { useForm } from "react-hook-form";
+import useStore from "@/app/store";
+import { uploadFile } from "@/app/lib/uploadFile";
 
 const petOptions = [
   { value: "Dog", label: "Dog" },
@@ -14,63 +15,70 @@ const petOptions = [
 ];
 
 const AddPetPopup = () => {
-    const { zId, zLogout } = useStore(); 
-
-  const [showModal, setShowModal] = useState(false);
-  const [showSuccessModal, setShowSuccessModal] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    species: "",
-    breed: "",
-    age: "",
-    sex: "",
-    color: "",
-    weight: "",
-    imageurl: "",
-    ownerid: zId,
-  });
-
-  const handleClose = () => {
-    setShowModal(false);
-    setFormData({
-      petName: "",
+  const { zId, zLogout } = useStore();
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      name: "",
       species: "",
       breed: "",
       age: "",
       sex: "",
-      colour: "",
+      color: "",
       weight: "",
       imageurl: "",
       ownerid: zId,
-    });
-  };
+    },
+  });
+  const [showModal, setShowModal] = useState(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+  const onSubmit = async (data) => {
+    const body = {
+      name: data.name,
+      species: data.species,
+      breed: data.breed,
+      age: data.age,
+      sex: data.sex,
+      color: data.color,
+      weight: data.weight,
+      ownerid: zId,
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const file = data.image[0];
+    let url = "";
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pets`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      url = await uploadFile(file);
+      console.log("Uploaded Image bruhh URL:", url)
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+
+    body["imageurl"] = url;
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pets`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        }
+      );
 
       const result = await response.json();
       console.log("Success:", result);
       setShowModal(false);
       setShowSuccessModal(true);
+      reset();
     } catch (error) {
       console.error("Error:", error);
     }
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    reset();
   };
 
   const closeSuccessModal = () => {
@@ -86,7 +94,7 @@ const AddPetPopup = () => {
         Add Pet
       </button>
 
-      {showModal ? (
+      {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-10 rounded-lg shadow-lg w-2/3 h-[600px] overflow-y-auto relative">
             <button
@@ -96,30 +104,26 @@ const AddPetPopup = () => {
               &times;
             </button>
 
-            <form className="space-y-4" onSubmit={handleSubmit}>
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="flex flex-col">
                   <label className="text-gray-700 font-bold mb-1">Pet Name</label>
                   <input
                     type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
+                    {...register("name", { required: true })}
                     className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
                     placeholder="Enter your pet's name"
-                    required
                   />
                 </div>
                 <div className="flex flex-col">
                   <label className="text-gray-700 font-bold mb-1">Pet Type</label>
                   <select
-                    name="species"
-                    value={formData.species}
-                    onChange={handleChange}
+                    {...register("species", { required: true })}
                     className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
-                    required
                   >
-                    <option value="" disabled>Select your pet's type</option>
+                    <option value="" disabled>
+                      Select your pet's type
+                    </option>
                     {petOptions.map((option) => (
                       <option key={option.value} value={option.value}>
                         {option.label}
@@ -131,60 +135,67 @@ const AddPetPopup = () => {
                   <label className="text-gray-700 font-bold mb-1">Breed</label>
                   <input
                     type="text"
-                    name="breed"
-                    value={formData.breed}
-                    onChange={handleChange}
+                    {...register("breed", { required: true })}
                     className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
                     placeholder="Enter the breed"
-                    required
                   />
                 </div>
                 <div className="flex flex-col">
                   <label className="text-gray-700 font-bold mb-1">Age (years)</label>
                   <input
                     type="number"
-                    name="age"
-                    value={formData.age}
-                    onChange={handleChange}
+                    {...register("age", { required: true })}
                     className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
                     placeholder="Enter the age"
-                    required
                   />
                 </div>
                 <div className="flex flex-col">
                   <label className="text-gray-700 font-bold mb-1">Sex</label>
-                  <input
-                    type="text"
-                    name="sex"
-                    value={formData.sex}
-                    onChange={handleChange}
-                    className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
-                    placeholder="Enter the sex"
-                    required
-                  />
+                  <div className="flex space-x-4">
+                    <label className="flex items-center">
+                      <input
+                        type="radio"
+                        {...register("sex", { required: true })}
+                        value="male"
+                        className="mr-2"
+                      />
+                      Male
+                    </label>
+                    <label className="flex items-center">
+                      <input
+                        type="radio"
+                        {...register("sex", { required: true })}
+                        value="female"
+                        className="mr-2"
+                      />
+                      Female
+                    </label>
+                  </div>
                 </div>
                 <div className="flex flex-col">
                   <label className="text-gray-700 font-bold mb-1">Color</label>
                   <input
                     type="text"
-                    name="color"
-                    value={formData.color}
-                    onChange={handleChange}
+                    {...register("color", { required: true })}
                     className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
-                    placeholder="Enter the color"
-                    required
+                    placeholder="Enter the colour"
                   />
                 </div>
                 <div className="flex flex-col">
                   <label className="text-gray-700 font-bold mb-1">Weight</label>
                   <input
-                    type="text"
-                    name="weight"
-                    value={formData.weight}
-                    onChange={handleChange}
+                    type="number"
+                    {...register("weight", { required: true })}
                     className="p-4 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
                     placeholder="Enter the weight"
-                    required
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="text-gray-700 font-bold mb-1">Upload Image</label>
+                  <input
+                    type="file"
+                    {...register("image", { required: true })}
+                    className="p-2 border rounded w-full bg-mybutton bg-opacity-35 shadow-md"
                   />
                 </div>
               </div>
@@ -199,23 +210,22 @@ const AddPetPopup = () => {
             </form>
           </div>
         </div>
-      ) : null}
-
-      {/* Success Modal */}
+      )}
       {showSuccessModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-10 rounded-lg shadow-lg w-1/3 text-center">
-            <h2 className="text-2xl font-bold text-green-600 mb-4">
-              Successfully Added!
-            </h2>
-            <button
-              className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 transition duration-300"
-              onClick={closeSuccessModal}
-            >
-              Close
-            </button>
-          </div>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="bg-white p-10 rounded-lg shadow-lg w-1/3 text-center">
+          <h2 className="text-2xl font-bold text-black-600 mb-4">
+            Successfully Submitted!
+          </h2>
+          <button
+            className="bg-mybutton text-black font-bold py-2 px-4 rounded-full"
+
+            onClick={closeSuccessModal}
+          >
+            Close
+          </button>
         </div>
+      </div>
       )}
     </div>
   );
